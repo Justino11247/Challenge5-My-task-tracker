b2100704841d58c79613506d0ebe8a8a894baf67
Justino <herrerajustin11@gmail.com>
2024-04-29 18:30:33 -0700

    Added form modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+  const timestamp = newDate().getTime(); // Gets current timestamp
+  const randomNum = Math.floor(Math.random() * 1000); // Generates random number
+  const taskID = `${timestamp}-${randomNum}`;
 }
 
 // Todo: create a function to create a task card
@@ -34,5 +36,14 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+  $('#taskDueDate').datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
 
+  // Make lanes droppable
+  $('.lane').droppable({
+    accept: '.draggable',
+    drop: handleDrop,
+  });
 });
